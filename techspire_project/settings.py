6680ee5055117c50dca6749a9bb915c116e46e64
Chiraj Exports <chiragexports1@gmail.com>
2026-09-05 14:21:36 +0530

    Optimize WSGI startup and simplify static storage for Vercel

--- techspire_project/settings.py
+++ techspire_project/settings.py
@@ -126,8 +126,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-WHITENOISE_USE_FINDERS = True
+
 
 
 # Media files (User avatars, Course thumbnails, Certificates)
